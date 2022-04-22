f5ef0b9a99017f1b6a7bcb5bb2fe9bf268dbb277
JasonChou <swchou524@gmail.com>
2022-04-22 23:50:54 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const app = express()
+const mongoose = require('mongoose')
 
 dotenv.config()
+const app = express()
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
